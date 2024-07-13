e11231891484a55b5685dc8ef209d4fb0fb23d10
AbhishekBhakari <bhakariabhi@gmail.com>
2024-07-13 18:53:45 +0530

    Day-1

---
+++ Day1/variablesAndDatatypes.js
@@ -0,0 +1,22 @@
+// Activity 1- Vairable Dicleration
+
+//Task 1 - 
+
+var greet = 'hi var';
+console.log(greet);
+
+//Task 2 - 
+
+let wish = 'hi let';
+console.log(wish);
+
+// Activity 1- Constant Dicleration
+
+//Task 1 - 
+
+const isUser = true;
+console.log(isUser);
+
+
+
+
