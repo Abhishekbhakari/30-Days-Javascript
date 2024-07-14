a71b1499e2433e604072016a38d0a827115f34bc
AbhishekBhakari <bhakariabhi@gmail.com>
2024-07-14 20:11:25 +0530

    Day-2

---
+++ Day2/Operators.js
@@ -0,0 +1,118 @@
+// Activity 1: Arithmetic Operations
+
+// Task 1: Write a program to add two numbers and log the result to the console.
+
+
+let addNum1 = 10;
+let addNum2 = 5;
+let sum = addNum1 + addNum2;
+console.log('Sum:', sum);
+
+
+// #### Task 2: Write a program to subtract two numbers and log the result to the console.
+
+let subNum1 = 10;
+let subNum2 = 5;
+let difference = subNum1 - subNum2;
+console.log('Difference:', difference);
+
+
+// #### Task 3: Write a program to multiply two numbers and log the result to the console.
+
+let mulNum1 = 10;
+let mulNum2 = 5;
+let product = mulNum1 * mulNum2;
+console.log('Product:', product);
+
+
+// #### Task 4: Write a program to divide two numbers and log the result to the console.
+
+let divNum1 = 10;
+let divNum2 = 5;
+let quotient = divNum1 / divNum2;
+console.log('Quotient:', quotient);
+
+
+// #### Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
+
+
+let remNum1 = 10;
+let remNum2 = 5;
+let remainder = remNum1 % remNum2;
+console.log('Remainder:', remainder);
+
+
+// ### Activity 2: Assignment Operators
+
+// #### Task 6: Use the operator to add a number to a variable and log the result to the console.
+
+let a = 10;
+a += 5; 
+console.log('Result after addition:', a);
+
+
+// #### Task 7: Use the operator to subtract a number from a variable and log the result to the console.
+
+let b = 10;
+b -= 5; 
+console.log('Result after subtraction:', b);
+
+
+// ### Activity 3: Comparison Operators
+
+// #### Task 8: Write a program to compare two numbers using `>` and log the result to the console.
+
+let x = 10;
+let y = 5;
+console.log('x > y:', x > y);
+
+
+// #### Task 9: Write a program to compare two numbers using `<` and log the result to the console.
+
+let c = 10;
+let d = 5;
+console.log('c < b:', a < b);
+
+
+// ### Activity 4: Logical Operators
+
+// #### Task 10: Write a program to compare two numbers using `>=` and `<=` and log the result to the console.
+
+let e = 10;
+let f = 5;
+console.log('e >= f:', e >= f);
+console.log('e <= f:', e <= f);
+
+
+// #### Task 11: Write a program that uses the `&&` operator to combine two conditions and log the result to the console.
+
+let g = 10;
+let h = 5;
+let i = 15;
+console.log('x > y && z > x:', g > h && i > g);
+
+
+// #### Task 12: Write a program that uses the `||` operator to combine two conditions and log the result to the console.
+
+
+let j = 10;
+let k = 5;
+let l = 15;
+console.log('x > y || z < y:', j > k || l < k);
+
+
+// #### Task 13: Write a program that uses the `!` operator to negate a condition and log the result to the console.
+
+
+let isRaining = false;
+console.log('!isRaining:', !isRaining);
+
+
+// ### Activity 5: Ternary Operator
+
+// #### Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+
+
+let num = -10;
+let result = num >= 0 ? 'Positive' : 'Negative';
+console.log('The number is:', result);
